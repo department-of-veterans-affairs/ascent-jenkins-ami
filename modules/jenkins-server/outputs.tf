11e967a909e28a05fa0a84e3645d186a7f000ef7
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-05 15:56:47 -0500

    PLAT-177: output some more stuff

--- modules/jenkins-server/outputs.tf
+++ modules/jenkins-server/outputs.tf
@@ -1,3 +1,12 @@
 output "jenkins_master_ip" {
   value = "${aws_instance.jenkins_master.private_ip}"
 }
+
+
+output "http_port" {
+  value = "${module.jenkins_security_group.http_port}"
+}
+
+output "jenkins_master_security_group_id" {
+  value = "${module.jenkins_master_security_group_id}"
+}
--- modules/security-groups/jenkins/outputs.tf
+++ modules/security-groups/jenkins/outputs.tf
@@ -1,3 +1,7 @@
 output "jenkins_master_security_group_id" {
-  value = "aws_security_group.jenkins.id"
+  value = "${aws_security_group.jenkins.id}"
+}
+
+output "http_port" {
+  value = "${var.http_access_port}"
 }
